fce4d880a91c6130c34b26c34a2150f61fb3b727
lilwani <40520365+lilwani@users.noreply.github.com>
2025-05-28 19:55:44 +0530

    Logo to navigate to menu

--- src/features/UI/Header.jsx
+++ src/features/UI/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { PhoneArrowDownLeftIcon } from '@heroicons/react/16/solid'
 import User from '../users/User'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header className="flex flex-row justify-between space-x-3 w-full px-4 bg-gray-500 border-2 rounded-xl py-4">
-            <div className="flex space-x-4 ">
-                <PhoneArrowDownLeftIcon className="h-10 w-10 text-blue-400" />
-                <p className="font-serif text-4xl tracking-wide text-white">
-                    Mobile Cart
-                </p>
+            <div>
+                <Link to="/products/menu" className="flex space-x-4 ">
+                    <PhoneArrowDownLeftIcon className="h-10 w-10 text-blue-400" />
+                    <p className="font-serif text-4xl tracking-wide text-white">
+                        Mobile Cart
+                    </p>
+                </Link>
             </div>
             <User />
         </header>
